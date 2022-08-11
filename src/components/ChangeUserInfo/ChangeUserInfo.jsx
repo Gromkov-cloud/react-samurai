c853a14b8ac73aa448751731eedb1711a130d655
Vasiliy Gromkov <v.gromkov2014@yandex.ru>
2022-08-11 21:15:29 +0500

    refactor: remove duplication contactsInput

--- src/components/ChangeUserInfo/ChangeUserInfo.jsx
+++ src/components/ChangeUserInfo/ChangeUserInfo.jsx
@@ -4,7 +4,6 @@ import {useDispatch, useSelector} from "react-redux";
 import {fetchUserInfoThunk, putUserInfoThunk} from "../../Redux/reduxSlices/changeUserInfoSlice";
 import styles from "./ChangeUserInfo.module.css"
 
-
 const ChangeUserInfo = () => {
     const dispatch = useDispatch()
     const ownerId = useSelector((state) => state.loginPage.userData.id)
@@ -20,62 +19,26 @@ const ChangeUserInfo = () => {
     } = useForm()
 
     const onFormSubmit = (data) => {
+        console.log(data)
         dispatch(putUserInfoThunk(data))
     }
 
-    setValue("vkLink", userContacts.vk)
-    setValue("githubLink", userContacts.github)
-    setValue("facebookLink", userContacts.facebook)
-    setValue("instagramLink", userContacts.instagram)
-    setValue("twitterLink", userContacts.twitter)
-    setValue("websiteLink", userContacts.website)
-    setValue("youtubeLink", userContacts.youtube)
+    Object.entries(userContacts).forEach(link => setValue(`${link[0]}Link`, link[1]))
 
     return (
         <>
             <form onSubmit={handleSubmit(onFormSubmit)} className={styles.form}>
-                <label>
-                    set your vk link:
-                    <input type="text"
-                           {...register("vkLink")}
-                    />
-                </label>
-                <label>
-                    set your github link:
-                    <input type="text"
-                           {...register("githubLink")}
-                    />
-                </label>
-                <label>
-                    set your facebook link:
-                    <input type="text"
-                           {...register("facebookLink")}
-                    />
-                </label>
-                <label>
-                    set your instagram link:
-                    <input type="text"
-                           {...register("instagramLink")}
-                    />
-                </label>
-                <label>
-                    set your twitter link:
-                    <input type="text"
-                           {...register("twitterLink")}
-                    />
-                </label>
-                <label>
-                    set your website link:
-                    <input type="text"
-                           {...register("websiteLink")}
-                    />
-                </label>
-                <label>
-                    set your youtube link:
-                    <input type="text"
-                           {...register("youtubeLink")}
-                    />
-                </label>
+                {
+                    Object.keys(userContacts).map(linkName => (
+                            <label key={linkName}>
+                                {`set your ${linkName} link:`}
+                                <input type="text"
+                                       {...register(`${linkName}Link`)}
+                                />
+                            </label>
+                        )
+                    )
+                }
                 <button type={"submit"}>submit</button>
             </form>
         </>
